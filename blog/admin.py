3e01a1efef79e93162f12284efc9dc5862860373
unvka017 <armiguel.03@gmail.com>
2025-06-14 16:35:34 -0400

    modelo de datos listo

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here my friend :3.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('created_at', 'author')
+    ordering = ('-created_at',)
+
+
+admin.site.register(Post, PostAdmin)
