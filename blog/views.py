78538a83747c2c4e1f62399f7fa023e6e6b5c5ce
unvka017 <armiguel.03@gmail.com>
2025-06-21 16:58:33 -0400

    detalle y enlace creado

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = 'post-list.html'
+
+# details view
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post-detail.html'
+    
+    
